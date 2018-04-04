295b385e17e94af1b86dde4c15e2520475ffcfc9
photodiode <arne@elqx.com>
2018-04-04 16:58:13 +0900

    Update to the preferences page

--- src/js/options.js
+++ src/js/options.js
@@ -1,4 +1,22 @@
 
+function new_element(name, attributes, children) {
+
+	const e = document.createElement(name);
+
+	for(const key in attributes) {
+		if(key == 'content') {
+			e.appendChild(document.createTextNode(attributes[key]));
+		}else{
+			e.setAttribute(key.replace(/_/g, '-'), attributes[key]);
+		}
+	}
+
+	for(const child of children || []) {
+		e.appendChild(child);
+	}
+
+	return e;
+}
 
 function formatByteSize(bytes) {
 	if(bytes < 1024) return bytes + " bytes";
@@ -7,13 +25,14 @@ function formatByteSize(bytes) {
 	else return(bytes / 1073741824).toFixed(3) + " GiB";
 };
 
-async function getThumbnailCacheSize() {
+async function getStatistics() {
 
-	const tabs = browser.tabs.query({});
+	const tabs = await browser.tabs.query({});
 
 	var totalSize = 0;
+	var numTabs = 0;
 
-	for(const tab of await tabs) {
+	for(const tab of tabs) {
 
 		var thumbnail = await browser.sessions.getTabValue(tab.id, 'thumbnail');
 
@@ -24,6 +43,9 @@ async function getThumbnailCacheSize() {
 
 	document.getElementById('thumbnailCacheSize').innerHTML = '';
 	document.getElementById('thumbnailCacheSize').appendChild(document.createTextNode(formatByteSize(totalSize)));
+
+	document.getElementById('numberOfTabs').innerHTML = '';
+	document.getElementById('numberOfTabs').appendChild(document.createTextNode(tabs.length));
 }
 
 /*function convertBackup(tgData) {
@@ -218,8 +240,21 @@ async function saveBackup() {
 	});
 }
 
-function init() {
-	getThumbnailCacheSize();
+async function init() {
+
+	var commands = await browser.commands.getAll();
+	var fragment = document.createDocumentFragment();
+
+	for(var i in commands) {
+		var key = new_element('button', {content: commands[i].shortcut, disabled: true, title: 'Will be customizable from Firefox v60'}, [])
+		var label = new_element('label', {content: commands[i].description, for: key}, []);
+
+		var commandNode = new_element('div', {}, [label, key]);
+		fragment.appendChild(commandNode);
+	}
+	document.getElementById('keyboardShortcuts').appendChild(fragment);
+
+	getStatistics();
 
 	//document.getElementById('backupFileInput').addEventListener('change', loadBackup);
 	document.getElementById('saveBackupButton').addEventListener('click', saveBackup);
