fa5aa8db0f9ea7da8a072ec3341686ff55278756
David Gurk <dgurk@medienwerft.de>
2019-12-17 20:28:21 +0100

    Set popup view with dark theme as default for the extension in v1

--- src/js/_share/options.js
+++ src/js/_share/options.js
@@ -1,16 +1,31 @@
-export const defaultOptions = {
-  theme: "light",
-  view: "freeform",
-  toolbarPosition: "top",
-  shortcut: {
-    "activate-next-group": {
-      disabled: false
-    },
-    "activate-previous-group": {
-      disabled: false
+export const defaultOptions = (() => {
+  const majorVersion = parseInt(browser.runtime.getManifest().version);
+  let options = {
+    toolbarPosition: "top",
+    shortcut: {
+      "activate-next-group": {
+        disabled: false
+      },
+      "activate-previous-group": {
+        disabled: false
+      }
     }
+  };
+
+  if (majorVersion >= 1) {
+    options = Object.assign(options, {
+      theme: "dark",
+      view: "popup",
+    });
+  } else {
+    options = Object.assign(options, {
+      theme: "light",
+      view: "freeform"
+    });
   }
-};
+
+  return options;
+})();
 
 /**
  * Return the current state of the options
