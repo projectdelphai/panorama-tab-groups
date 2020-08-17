4b85db8d4aa1742a684e17b82519a6bf561f9f8b
Reuben Castelino <projectdelphai@gmail.com>
2020-08-16 20:10:27 -0700

    added initial test infrastructure for testing ptg. Started with web-ext lint and eslint options

---
+++ .eslintrc.js
@@ -0,0 +1,19 @@
+module.exports = {
+  "env": {
+    browser: true,
+    es2020: true,
+  },
+  "extends": [
+    "airbnb-base",
+    "plugin:json/recommended"
+  ],
+  "plugins": [
+      "json"
+  ],
+  "parserOptions": {
+    ecmaVersion: 11,
+    sourceType: "module",
+  },
+  "rules": {
+  },
+};
